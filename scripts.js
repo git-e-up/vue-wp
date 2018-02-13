0c6514c848457eda2eef1c512678bb1411ed7add
Matt Lissner <matt@doejo.com>
2018-02-13 17:29:44 -0600

    ssl warning thing

--- scripts.js
+++ scripts.js
@@ -1,46 +1,23 @@
 var vm = new Vue({
   el: '#app',
   data: {
-    items: [
-      // { message: 'Foo' },
-      // { message: 'Bar' }
-    ]
+    items: [],
+    isActive: false
   },
   mounted: function(){
     this.$http.get('http://matthewlissner.com/wp-json/wp/v2/hot_sauces?filter[orderby]=date&order=asc').then(response => {
-
-      // get body data
-      // console.log(response.body[1].date);
-      // this.$set(this.origin, me)
-      // this.items = response.body[0].title.rendered;
       let x = this;
       response.body.forEach(function(i){
         x.items.push({message: i.title.rendered})
-
-        // x.items.message = i.title.rendered;
-        // console.log(i.title.rendered);
       })
-
     }, response => {
-      console.log('no');
+      console.log('sorry about that');
+      this.isActive = true;
       // error callback
     });
   }
 })
 
-// {
-//   // GET /someUrl
-//   Vue.http.get('http://matthewlissner.com/wp-json/wp/v2/posts?filter[orderby]=date&order=asc').then(response => {
-//
-//     // get body data
-//     Vue.someData = response.body;
-//     console.log(response.body[1]);
-//
-//   }, response => {
-//     console.log('no');
-//     // error callback
-//   });
-// }
 
 Vue.component('my-component', {
   template: '<div>A customz component!</div>'
