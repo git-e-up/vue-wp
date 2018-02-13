3eea0c16c9314ebb7e05f6e8ecc3c22a07cc01ab
Matt Lissner <matt@doejo.com>
2018-02-13 15:32:46 -0600

    basic loop outputting titles from api response

--- scripts.js
+++ scripts.js
@@ -1,20 +1,70 @@
-var app = new Vue({
+var vm = new Vue({
   el: '#app',
-    data: {
-    message: 'Helloz'
+  data: {
+    items: [
+      // { message: 'Foo' },
+      // { message: 'Bar' }
+    ]
+  },
+  mounted: function(){
+    this.$http.get('http://matthewlissner.com/wp-json/wp/v2/hot_sauces?filter[orderby]=date&order=asc').then(response => {
+
+      // get body data
+      // console.log(response.body[1].date);
+      // this.$set(this.origin, me)
+      // this.items = response.body[0].title.rendered;
+      let x = this;
+      response.body.forEach(function(i){
+        x.items.push({message: i.title.rendered})
+
+        // x.items.message = i.title.rendered;
+        // console.log(i.title.rendered);
+      })
+
+    }, response => {
+      console.log('no');
+      // error callback
+    });
   }
 })
 
-{
-  // GET /someUrl
-  Vue.http.get('http://matthewlissner.com/wp-json/wp/v2/posts?filter[orderby]=date&order=asc').then(response => {
+// {
+//   // GET /someUrl
+//   Vue.http.get('http://matthewlissner.com/wp-json/wp/v2/posts?filter[orderby]=date&order=asc').then(response => {
+//
+//     // get body data
+//     Vue.someData = response.body;
+//     console.log(response.body[1]);
+//
+//   }, response => {
+//     console.log('no');
+//     // error callback
+//   });
+// }
 
-    // get body data
-    Vue.someData = response.body;
-    console.log(response.body[1]);
+Vue.component('my-component', {
+  template: '<div>A customz component!</div>'
+})
+new Vue({
+  el: '#example'
+})
 
-  }, response => {
-    console.log('no');
-    // error callback
-  });
-}
+new Vue({
+    el: '#my_view',
+    data: {
+       origin: ''
+    },
+
+    ready: function() {
+
+        // GET request
+        this.$http.get('http://httpbin.org/ip', function (data) {
+            // set data on vm
+            this.$set('origin', data)
+
+        }).error(function (data, status, request) {
+            // handle error
+        })
+
+      }
+})
