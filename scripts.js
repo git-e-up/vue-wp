7f23a66deb50ddeb96c33b8919bdbb67d0d945b5
Matt Lissner <matt@doejo.com>
2018-02-14 16:56:47 -0600

    get index in data attr

--- scripts.js
+++ scripts.js
@@ -7,8 +7,8 @@ var vm = new Vue({
   mounted: function(){
     this.$http.get('http://matthewlissner.com/wp-json/wp/v2/hot_sauces?filter[orderby]=date&order=asc').then(response => {
       let x = this;
-      response.body.forEach(function(i){
-        x.items.push({message: i.title.rendered})
+      response.body.forEach(function(val, i){
+        x.items.push({message: val.title.rendered})
       })
     }, response => {
       console.log('sorry about that');
